6dbb0de77ef31a52ae6e9dc94cecdf804f3de273
Kyle Wenzel <33awesomekooldude@gmail.com>
2020-09-08 15:23:01 -0500

    Add files via upload

---
+++ Chapter 1/Name.py
@@ -0,0 +1,12 @@
+##Name: Kyle Wenzel
+##Date: 9/8/2020
+##Program Title: Name.py
+##Program Description: prints my name
+
+##Step 1: Print my name
+
+print("Kyle Wenzel")
+
+##Step 2: Wait for exit command
+
+input("\nPress enter to exit...")
