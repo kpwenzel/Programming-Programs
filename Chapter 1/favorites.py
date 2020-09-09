010e4e8d73b0555321100c927374b22e0f26daa5
Kyle Wenzel <33awesomekooldude@gmail.com>
2020-09-09 10:40:01 -0500

    Add files via upload

---
+++ Chapter 1/favorites.py
@@ -0,0 +1,13 @@
+# Kyle Wenzel
+# September 3, 2020
+# Favorites Program
+# This program will display my favorites
+
+# Step 1: Displays the favorites
+print("\n{:^60}".format("My Favorite Things"))
+print("{:<30}{:<30}".format("Favorite Color:", "Red"))
+print("{:<30}{:<30}".format("Favorite Food:", "Deep dish pizza"))
+print("{:<30}{:<30}".format("Favorite Movie:", "Sonic the hedgehog"))
+print("{:<30}{:<30,.2f}".format("Favorite Numbher:", 4987))
+
+input("\n\nPress Enter to exit...")
---
+++ Chapter 1/school song.py
@@ -0,0 +1,12 @@
+# Kyle Wenzel
+# September 3, 2020
+# School SOng Program
+# This program will display the lyrics to the school song
+
+# Step 1: display the lyrics
+print("All hail our school, NCHS")
+print("We'll sing your praises, NCHS")
+print("With all our might")
+print("We'll always make a fight")
+
+input("\nPress enter to exit.")
